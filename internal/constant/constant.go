03ffe770c44f0dcadff2bf78624f7922cffca87d
kalom60 <39136564+kalom60@users.noreply.github.com>
2026-01-04 02:17:13 +0300

    contants

---
+++ internal/constant/constant.go
@@ -0,0 +1,7 @@
+package constant
+
+const (
+	PAYMENT_CREATED   = "Payment created successfully"
+	PAYMENT_RETRIEVED = "Payment retrieved successfully"
+	PAYMENT_UPDATED   = "Payment updated successfully"
+)
