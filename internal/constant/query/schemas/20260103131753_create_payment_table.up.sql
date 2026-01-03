a2f477d7ba2977f4411687945c5a532f0c2634e9
kalom60 <39136564+kalom60@users.noreply.github.com>
2026-01-03 17:13:27 +0300

    payments schema

---
+++ internal/constant/query/schemas/20260103131753_create_payment_table.up.sql
@@ -0,0 +1,20 @@
+CREATE TYPE payment_currency AS ENUM (
+    'ETB',
+    'USD'
+);
+
+CREATE TYPE payment_status AS ENUM (
+    'PENDING',
+    'SUCCESS',
+    'FAILED'
+);
+
+CREATE TABLE IF NOT EXISTS payments (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    reference UUID NOT NULL,
+    amount NUMERIC(18,2) NOT NULL,
+    currency payment_currency NOT NULL,
+    status payment_status NOT NULL,
+    created_at TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+    updated_at TIMESTAMP WITHOUT TIME ZONE NOT NULL
+);
