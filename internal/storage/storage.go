47e43291af704572567e5a2e437191ab0fc80f65
kalom60 <39136564+kalom60@users.noreply.github.com>
2026-01-04 00:24:27 +0300

    payment interface

---
+++ internal/storage/storage.go
@@ -0,0 +1,14 @@
+package storage
+
+import (
+	"context"
+
+	"github.com/google/uuid"
+	"github.com/kalom60/cashflow/internal/constant/dto"
+)
+
+type Payment interface {
+	CreatePayment(ctx context.Context, payment dto.Payment) (dto.Payment, error)
+	GetPaymentByID(ctx context.Context, id uuid.UUID) (dto.Payment, error)
+	UpdatePaymentStatus(ctx context.Context, id uuid.UUID, status dto.PaymentStatus) error
+}
