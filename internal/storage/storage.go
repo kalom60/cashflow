ed8540711018a3b938223b19de3533a6357af769
kalom60 <39136564+kalom60@users.noreply.github.com>
2026-01-04 15:20:14 +0300

    add outbox event

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -12,3 +12,9 @@ type Payment interface {
 	GetPaymentByID(ctx context.Context, id uuid.UUID) (dto.Payment, error)
 	UpdatePaymentStatus(ctx context.Context, id uuid.UUID, status dto.PaymentStatus) error
 }
+
+type OutboxEvent interface {
+	GetPendingOutboxEventsForUpdate(ctx context.Context) ([]dto.OutboxEvent, error)
+	UpdateOutboxStatus(ctx context.Context, id uuid.UUID, status dto.OutboxStatus) error
+	DeleteOutboxEvent(ctx context.Context, id uuid.UUID) error
+}
